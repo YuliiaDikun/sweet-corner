7a8b0516abff1d22a700bfefb2a69e16f59992de
Yuliia Dikun <dikun.yulia@gmail.com>
2023-03-01 21:12:46 +0200

    feat: logout

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -10,9 +10,15 @@ import {
 import { AiOutlineShoppingCart } from "react-icons/ai";
 import { RxHamburgerMenu } from "react-icons/rx";
 import { MdClose } from "react-icons/md";
+import { signOut } from "firebase/auth";
+import { auth } from "../../firebase/config";
+import { Loader } from "../../components";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const Navigation = () => {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
   const toggleMenu = () => {
     setIsMobileMenuOpen(!isMobileMenuOpen);
@@ -20,6 +26,18 @@ const Navigation = () => {
 
   const hideMenu = () => {
     setIsMobileMenuOpen(false);
+  };
+
+  const logoutUser = () => {
+    setIsLoading(true);
+    signOut(auth)
+      .then(() => {
+        toast.success("Bye! Come back to us soon.");
+      })
+      .catch((error) => {
+        toast.error(error.message);
+      })
+      .finally(() => setIsLoading(false));
   };
 
   const shopItems = 0;
@@ -30,38 +48,41 @@ const Navigation = () => {
   );
   const logo = <Logo to="/">NONAME SHOP</Logo>;
   return (
-    <HederWrapper>
-      {logo}
-      <StyledNav open={isMobileMenuOpen}>
-        <NavWrapper open={isMobileMenuOpen} onClick={hideMenu}></NavWrapper>
-        <ul onClick={hideMenu}>
-          <li>{logo}</li>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/login">Login</Link>
-          </li>
-          <li>
-            <Link to="/register">Register</Link>
-          </li>
-          <li>
-            <Link to="/" onClick={logoutUser}>
-              Logout
-            </Link>
-          </li>
-          <li>{cart}</li>
-        </ul>
-      </StyledNav>
-      <MobileWrapper>
-        {cart}
-        {isMobileMenuOpen ? (
-          <MdClose size={20} onClick={toggleMenu} />
-        ) : (
-          <RxHamburgerMenu size={20} onClick={toggleMenu} />
-        )}
-      </MobileWrapper>
-    </HederWrapper>
+    <>
+      {isLoading && <Loader />}
+      <HederWrapper>
+        {logo}
+        <StyledNav open={isMobileMenuOpen}>
+          <NavWrapper open={isMobileMenuOpen} onClick={hideMenu}></NavWrapper>
+          <ul onClick={hideMenu}>
+            <li>{logo}</li>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/register">Register</Link>
+            </li>
+            <li>
+              <Link to="/" onClick={logoutUser}>
+                Logout
+              </Link>
+            </li>
+            <li>{cart}</li>
+          </ul>
+        </StyledNav>
+        <MobileWrapper>
+          {cart}
+          {isMobileMenuOpen ? (
+            <MdClose size={20} onClick={toggleMenu} />
+          ) : (
+            <RxHamburgerMenu size={20} onClick={toggleMenu} />
+          )}
+        </MobileWrapper>
+      </HederWrapper>
+    </>
   );
 };
 
